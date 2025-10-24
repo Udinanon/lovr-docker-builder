def5c8a52632c3565abf1e233eb8187e30864d5a
Udinanon <m.trapanotto@tutamail.com>
2025-10-24 12:08:12 +0200

    Moved build folder creation to docker script Docker container name is now dynamic based on folder name, easier for multiple jobs in parallel If the project foler is empty the tag is now cloned there and not at cwd Uses new v3 container, working with lovr v17+

--- build_android.sh
+++ build_android.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/sh
 set -e # fail on any command error
+set -u # fail on unset vairables
+set -o pipefail # pass the failing command error instead of the last one
 
 # VARIABLES
 PROJECT_PATH=/home/udinanon/Programming/Projects/LOVR/misc/compile-test # Your code folder
-CONTAINER_NAME=lovr_android:v2 # The name of the built docker continer
+CONTAINER_NAME=lovr_android:v3 # The name of the built docker container
 KEYSTORE_PATH=/home/udinanon/Programming/Projects/LOVR/misc/lovr_keys.keystore # Your keystore
 KEYSTORE_PASS=vrjams # Keystore password
 BRANCH_TAG=v0.16.0 # If folder empty, version to clone and compile
@@ -12,26 +14,17 @@ BRANCH_TAG=v0.16.0 # If folder empty, version to clone and compile
 echo "${PROJECT_PATH} as target folder"
 if [ "$(ls -A ${PROJECT_PATH})" ]; then
     echo "Folder not empty, compiling on it"
-    cd ${PROJECT_PATH} 
-
 else 
     echo "Folder empty, cloning target version inside first"
-    cd ${PROJECT_PATH}
-    git clone --depth 1 --branch  ${BRANCH_TAG} --single-branch --recurse-submodules --shallow-submodules https://github.com/bjornbytes/lovr.git .
+    git clone --depth 1 --branch  ${BRANCH_TAG} --single-branch --recurse-submodules --shallow-submodules https://github.com/bjornbytes/lovr.git ${PROJECT_PATH}
 fi
-
-if [ ! -d "${PROJECT_PATH}/build" ]; then
-    echo "Creating build folder."
-    mkdir build
-fi
-
-cd ${PROJECT_PATH}/build
 
 # Step 2: load Docker
-DOCKER_NAME=android_builder
+FOLDER_NAME=$(basename "$PROJECT_PATH") # Extrasct last folder name from path, so works both with branch tags and a custom folder
+DOCKER_NAME="${FOLDER_NAME}_android_builder"
 docker run -d --name ${DOCKER_NAME} -v ${PROJECT_PATH}:/root/code -w /root/ ${CONTAINER_NAME}
 docker cp ${KEYSTORE_PATH} ${DOCKER_NAME}:/root/keys.keystore
-docker cp android/cmake_commands.sh ${DOCKER_NAME}:/root/cmake_commands.sh
+docker cp ./android/cmake_commands.sh ${DOCKER_NAME}:/root/cmake_commands.sh
 
 # Step 3: launch compialtion
 docker exec -e KEYSTORE_PASS=${KEYSTORE_PASS} ${DOCKER_NAME} /root/cmake_commands.sh 
